d29b94403f0796240cb9897de9b5c8d71c13a535
rotaliator <rotaliator@gmail.com>
2020-12-09 13:45:21 +0100

    day8 part 2

--- src/adventofcode2020/day8.clj
+++ src/adventofcode2020/day8.clj
@@ -2,20 +2,16 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
+(def test-input (-> "day8_sample.txt" io/resource io/reader line-seq))
 (def input (-> "day8.txt" io/resource io/reader line-seq))
-(def test-input (-> "nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6" str/split-lines))
 
-(def parsed-input (->> input
-                       (map #(str/split % #"\s"))
-                       (mapv (fn [[op n]] [(keyword op) (read-string n)]))))
+(defn parse-input [input]
+  (->> input
+       (map #(str/split % #"\s"))
+       (mapv (fn [[op n]] [(keyword op) (read-string n)]))))
+
+(def parsed-test-input (parse-input test-input))
+(def parsed-input (parse-input input))
 
 (defn run [input]
   (loop [a   0
@@ -35,3 +31,60 @@ acc +6" str/split-lines))
   (run parsed-input)
 ;; => 1814
   )
+
+;; part 2
+
+(defn run-repaired [input]
+  (loop [a   0
+         i   0
+         ins #{}]
+    (let [[opcode value] (get input i [nil nil])]
+      (if opcode
+        (if-not (contains? ins i)
+          (case opcode
+            :nop (recur a           (inc i)     (conj ins i))
+            :jmp (recur a           (+ i value) (conj ins i))
+            :acc (recur (+ a value) (inc i)     (conj ins i)))
+          nil)
+        a))))
+
+(def fix {:jmp :nop
+          :nop :jmp})
+
+(defn fix-at-instr [input i]
+  (update-in input [i 0] fix))
+
+(defn repeirable [input]
+  (->> input
+       (map-indexed vector)
+       (filter (comp #{:nop :jmp} first second))
+       (mapv first)))
+
+(defn run-with-fix [input]
+  (->> (map (comp run-repaired (partial fix-at-instr input))
+            (repeirable input))
+       (filter identity)
+       (first)))
+
+(comment
+  (run-with-fix parsed-test-input)
+
+  (run-with-fix parsed-input)
+;; => 1056
+  )
+
+
+
+
+
+
+
+
+
+
+(fix-at-instr 7)
+
+
+
+
+(run-repaired (fix-at-instr 7))
